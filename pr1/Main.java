e0050c574faec8bef946549a66138e5083a0243c
Kherdu <rafacaturla@gmail.com>
2014-11-16 10:54:23 +0100

    al fiiiin!
    arreglao todo, pasa los test, a entregar

--- pr1/Main.java
+++ pr1/Main.java
@@ -1,7 +1,5 @@
 package tp.pr1;
 
-
-
 import java.util.Scanner;
 
 import tp.pr1.control.Controlador;
@@ -11,13 +9,12 @@ import tp.pr1.logica.Partida;
 public class Main {
 
 	public static void main(String[] args) {
-		
-			Scanner in = new Scanner(System.in);
-			Partida p=new Partida();
-			Controlador C=new Controlador(p,in);
-			C.run();
-			
-			
+
+		Scanner in = new Scanner(System.in);
+		Partida p = new Partida();
+		Controlador C = new Controlador(p, in);
+		C.run();
+
 	}
 
 }
