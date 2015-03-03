b8380fcb5e61a1a8f2b08e414ea699656df8b6e8
Hippie <jorfer03@ucm.es>
2015-03-03 16:59:40 +0100

    Reglas Gravity
    Reglas G creo que completas.

--- pr3/logica/ReglasGravity.java
+++ pr3/logica/ReglasGravity.java
@@ -25,34 +25,184 @@ public class ReglasGravity implements ReglasJuego{
 		this.ganador = Ficha.VACIA;
 		this.tipo = Juego.GRAVITY;
 	}
+	
 	@Override
 	public Ficha hayGanador(Movimiento ultimoMovimiento, Tablero t) {
-		// TODO Auto-generated method stub
-		return null;
+		// sacar ganador, comprobar vertical, horizontal y DIAGONAL.
+				ganador = Ficha.VACIA;
+				int cont = 0;
+
+				// vertical
+				for (int j = 1; j <= t.getAncho() && cont < 4 && !isGanador(); j++) {
+					cont = 0;// reiniciamos contador
+
+					for (int i = 1; i <= t.getAlto() && cont < 4 && !isGanador(); i++) {
+						if (t.getCasilla(j, i) != ultimoMovimiento.getJugador())
+							cont = 0;
+						else
+							cont++;
+						if (cont == 4) {
+							ganador = ultimoMovimiento.getJugador();
+
+						}
+					}
+				}
+
+				// horizontales
+				for (int i = 1; i <= t.getAlto() && cont < 4 && !isGanador(); i++) {
+					cont = 0;// reiniciamos contador
+
+					for (int j = 1; j <= t.getAncho() && cont < 4 && !isGanador(); j++) {
+						if (t.getCasilla(j, i) != ultimoMovimiento.getJugador())
+							cont = 0;
+						else
+							cont++;
+						if (cont == 4) {
+							ganador = ultimoMovimiento.getJugador();
+
+						}
+					}
+				}
+
+				// diagonales hacia la derecha \
+				// diagonales que empiezan con j=1
+				for (int j = 1; j <= (t.getAncho() - 3) && cont < 4 && !isGanador(); j++) {
+					int i = 1;// Doble indice para saltar por la diagonal
+					int j2 = j;
+					cont = 0;
+					while (i <= t.getAlto() && j2 <= t.getAncho() && cont < 4
+							&& !isGanador()) {
+
+						if (t.getCasilla(j2, i) != ultimoMovimiento.getJugador())
+							cont = 0;
+						else
+							cont++;
+						if (cont == 4) {
+							ganador = ultimoMovimiento.getJugador();
+
+						}
+						i++;
+						j2++;
+					}
+				}
+
+				// diagonales hacia la derecha \
+				// diagonales con i=1
+				for (int i = 1; i <= (t.getAlto() - 3) && cont < 4 && !isGanador(); i++) {
+					int i2 = i;// Doble indice para saltar por la diagonal
+					int j = 1;
+					cont = 0;
+					while (i2 <= t.getAlto() && j <= t.getAncho() && cont < 4
+							&& !isGanador()) {
+
+						if (t.getCasilla(j, i2) != ultimoMovimiento.getJugador())
+							cont = 0;
+						else
+							cont++;
+						if (cont == 4) {
+							ganador = ultimoMovimiento.getJugador();
+
+						}
+						i2++;
+						j++;
+					}
+				}
+
+				// diagonales hacia la izquierda /
+				// diagonales con j=1
+				for (int j = t.getAncho(); j >= (1 + 3) && cont < 4 && !isGanador(); j--) {
+					int i = 1;// Doble indice para saltar por la diagonal
+					int j2 = j;
+					cont = 0;
+					while (i <= t.getAlto() && j2 >= 1 && cont < 4 && !isGanador()) {
+						if (t.getCasilla(j2, i) != ultimoMovimiento.getJugador())
+							cont = 0;
+						else
+							cont++;
+						if (cont == 4) {
+							ganador = ultimoMovimiento.getJugador();
+
+						}
+						i++;
+						j2--;
+					}
+				}
+				// diagonales hacia la izquierda /
+				// diagonales con j=7
+				for (int i = 1; i <= (t.getAlto() - 3) && cont < 4 && !isGanador(); i++) {
+					int i2 = i;// Doble indice para saltar por la diagonal
+					int j = t.getAncho();
+					cont = 0;
+					while (i2 <= t.getAlto() && j >= 1 && cont < 4 && !isGanador()) {
+						if (t.getCasilla(j, i2) != ultimoMovimiento.getJugador())
+							cont = 0;
+						else
+							cont++;
+						if (cont == 4) {
+							ganador = ultimoMovimiento.getJugador();
+
+						}
+						i2++;
+						j--;
+					}
+				}
+
+				if (ganador != Ficha.VACIA) {
+
+					ganador = ultimoMovimiento.getJugador();
+				}
+				return ganador;
 	}
 
 	@Override
 	public Tablero iniciaTablero() {
-		// TODO Auto-generated method stub
-		return null;
+		this.tablero = new Tablero(ancho, alto);
+		return tablero;
 	}
 
 	@Override
 	public Ficha jugadorInicial() {
-		// TODO Auto-generated method stub
-		return null;
+	
+		return Ficha.BLANCA;
 	}
 
 	@Override
 	public Ficha siguienteTurno(Ficha ultimoEnPoner, Tablero t) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Ficha ret = Ficha.VACIA;
+		if (ultimoEnPoner == Ficha.BLANCA) {
+
+			ret = Ficha.NEGRA;
+
+		} else if (ultimoEnPoner == Ficha.NEGRA) {
+
+			ret = Ficha.BLANCA;
+
+		}
+
+		return ret;
 	}
 
 	@Override
 	public boolean tablas(Ficha ultimoEnPoner, Tablero t) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean ret = true;
+		
+		for (int i = 1; i <= t.getAncho(); i++){
+			
+			for (int j = 1; j <= t.getAlto(); j++){
+				
+				if (t.getCasilla(i, j) == Ficha.VACIA){
+					
+					ret = false;
+					
+				}
+				
+			}
+			
+			
+		}
+		
+		return ret;
 	}
 
 	@Override
@@ -67,8 +217,15 @@ public class ReglasGravity implements ReglasJuego{
 
 	@Override
 	public Juego getTipo() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return tipo;
 	}
 
+	public boolean isGanador() {
+		boolean ret = false;
+
+		if (ganador != Ficha.VACIA)
+			ret = true;
+		return ret;
+	}
 }
