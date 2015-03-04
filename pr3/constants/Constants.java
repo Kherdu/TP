b55c6c943caf7d6d2b35aeb38ba84eb817bb9277
Kherdu <rafacaturla@gmail.com>
2015-03-04 14:06:50 +0100

    controlador
    mas o menos está, faltara decididr mensajes de error

--- pr3/constants/Constants.java
+++ pr3/constants/Constants.java
@@ -6,6 +6,13 @@ package tp.pr3.constants;
  */
 public class Constants {
 
+	public static final String MensajeAyuda = "Los comandos disponibles son:\n"
+			+ "\nPONER: utilízalo para poner la siguiente ficha."
+			+ "\nDESHACER: deshace el último movimiento hecho en la partida."
+			+ "\nREINICIAR: reinicia la partida."
+			+ "\nJUGAR [c4|co|gr] [tamX tamY]: cambia el tipo de juego."
+			+ "\nJUGADOR [blancas|negras] [humano|aleatorio]: cambia el tipo de jugador."
+			+ "\nSALIR: termina la aplicación.\nAYUDA: muestra esta ayuda.";
 	public static String cosa;
 	public static int AnchoC4=7;
 	public static int AltoC4=6;
--- pr3/control/Controlador.java
+++ pr3/control/Controlador.java
@@ -2,7 +2,9 @@ package tp.pr3.control;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
+import tp.pr3.constants.Constants;
 import tp.pr3.logica.Ficha;
 import tp.pr3.logica.Juego;
 import tp.pr3.logica.Movimiento;
@@ -12,29 +14,30 @@ import tp.pr3.logica.MovimientoInvalido;
 import tp.pr3.logica.Partida;
 import tp.pr3.logica.ReglasComplica;
 import tp.pr3.logica.ReglasConecta4;
+import tp.pr3.logica.ReglasGravity;
+import tp.pr3.logica.ReglasJuego;
 
 public class Controlador {
 
 	private Partida partida;
 	private Scanner in;
-	private Movimiento m;
 	private FactoriaTipoJuego f;
 	private Jugador jugador1;
 	private Jugador jugador2;
 	private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+	private ReglasJuego reglas;
 
-	public Controlador(FactoriaTipoJuego factoria,Partida partida, Scanner sc) {
+	public Controlador(FactoriaTipoJuego factoria, Partida partida, Scanner sc) {
 		this.partida = partida;
-		this.f=factoria;
-		this.in=sc;
-		this.jugador1= f.creaJugadorHumanoConsola(in);
-		this.jugador2= f.creaJugadorHumanoConsola(in);
+		this.f = factoria;
+		this.in = sc;
+		this.jugador1 = f.creaJugadorHumanoConsola(in);
+		this.jugador2 = f.creaJugadorHumanoConsola(in);
 		this.jugadores.add(jugador1);
 		this.jugadores.add(jugador2);
-	
+		this.reglas = f.creaReglas();
+
 	}
-
-
 
 	public void run() {
 
@@ -61,46 +64,79 @@ public class Controlador {
 	}
 
 	public void parse(String s) {
-		// parser-ejecucion, cambiar para que lance excepciones... en ejecutamovimiento deberia lanzarlas cuando sea fuera del tablero, en sitio ocupado y cuando esta finalizada la partida
-		
-		if (s.compareToIgnoreCase("poner") == 0) {
-			
-			try{
-				for (Jugador j: jugadores){
-					
-					partida.Mover(j, in);
-				
+		// parser-ejecucion, cambiar para que lance excepciones... en
+		// ejecutamovimiento deberia lanzarlas cuando sea fuera del tablero, en
+		// sitio ocupado y cuando esta finalizada la partida
+
+		StringTokenizer st = new StringTokenizer(s);
+		if (st.hasMoreTokens()) {
+			String aux = st.nextToken(" ");
+			if (st.countTokens() == 1) {
+
+				if ((aux.compareToIgnoreCase("poner")) == 0) {
+					try {
+						for (Jugador j : jugadores) {
+							partida.Mover(j, in);
+						}
+					} catch (MovimientoInvalido e) {
+						e.printStackTrace();
+					}
+				} else if (aux.compareToIgnoreCase("reiniciar") == 0) {
+					partida.reset(f.creaReglas());
+					System.out.print("Partida reiniciada.");
+
+				} else if (aux.compareToIgnoreCase("salir") == 0) {
+
+					System.exit(0);
+
+				} else if (aux.compareToIgnoreCase("deshacer") == 0) {
+					if (!partida.undo())
+						System.err.println("Imposible deshacer.");
+				} else if (aux.compareToIgnoreCase("ayuda") == 0) {
+
+					System.out.println(Constants.MensajeAyuda);
+				} else System.out.println("controlador 98");
+
+			} else if (st.countTokens() == 2) {
+
+				if (aux.compareToIgnoreCase("jugar") == 0) {
+					String ju = st.nextToken();
+
+					if (ju.compareToIgnoreCase("c4") == 0) {
+						f = new FactoriaConecta4();
+						reglas = f.creaReglas();
+						partida.reset(reglas);
+						System.out.println("Partida reiniciada.");
+					} else if (ju.compareToIgnoreCase("co") == 0) {
+						f = new FactoriaComplica();
+						reglas = f.creaReglas();
+						System.out.println("Partida reiniciada.");
+					} else if (ju.compareToIgnoreCase("gr") == 0) {
+						f = new FactoriaGravity();
+						reglas = f.creaReglas();
+						System.out.println("Partida reiniciada.");
+					}
+				}else System.out.println("controlador 119");
+
+			} else if (st.countTokens() == 4) {
+
+				if (aux.compareToIgnoreCase("jugar") == 0) {
+					String ju = st.nextToken();
+					if (ju.compareToIgnoreCase("gr") == 0) {
+						int c = 0;
+						int fi = 0;
+						try {
+							c = Integer.parseInt(st.nextToken(" "));
+							fi = Integer.parseInt(st.nextToken(" "));
+						} catch (NumberFormatException e) {
+							System.err.println("NO");
+						}
+
+						f = new FactoriaGravity(fi, c);
+						System.out.println("Partida reiniciada.");
+					}
 				}
-				
-			}catch (MovimientoInvalido e){
-					e.printStackTrace();
-			}
-				
-			}
-		 else if (s.compareToIgnoreCase("reiniciar") == 0) {
-			partida.reset(f.creaReglas());
-			System.out.print("Partida reiniciada.");
-
-		} else if (s.compareToIgnoreCase("salir") == 0) { 
-
-			System.exit(0);
-
-		} else if (s.compareToIgnoreCase("deshacer") == 0) {
-			if (!partida.undo())
-				System.err.println("Imposible deshacer.");
-
-		} else if (s.compareToIgnoreCase("jugar c4") == 0) {
-			ReglasConecta4 reglas = new ReglasConecta4();
-			partida.reset(reglas);
-			System.out.println("Partida reiniciada.");
-		} else if (s.compareToIgnoreCase("jugar co") == 0) {
-			ReglasComplica reglas = new ReglasComplica();
-			partida.reset(reglas);
-			System.out.println("Partida reiniciada.");
-
-		} else
-			System.err.println("No te entiendo.");
-
+			}else System.out.println("controlador 139");
+		}else System.out.println("controlador 140");
 	}
-
 }
