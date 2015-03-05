b4b76017a0832322107c19f4abaa4ff4845af382
Kherdu <rafacaturla@gmail.com>
2015-03-05 16:43:36 +0100

    main
    cambiando cosas, no toques

--- pr3/Main.java
+++ pr3/Main.java
@@ -14,15 +14,74 @@ import tp.pr3.logica.ReglasJuego;
 
 public class Main {
 
-	public static int main(String[] args) {
+	public static void main(String[] args) {
 
-		int salida=0;
+		
 		int columnNumber = 0;
 		int rowNumber = 0;
 		String game = null;
 		CommandLineParser parser = null;
 		CommandLine cmdLine = null;
+		Options options = generateOptions();
+		boolean ok=false;
+		
+		FactoriaTipoJuego f = new FactoriaConecta4();
+		ReglasJuego reglas = f.creaReglas();
+		Scanner in = new Scanner(System.in);
+		Partida p = new Partida(reglas);
+		Controlador c = new Controlador(f, p, in);
+		
+		// aqui irian los optionGroup para incompatibilidades de argumentos
+		if (args == null || args.length < 1 || args[0] == null) {
+			ok=true;
+		} else
+			try {
+				parser = new BasicParser();
+				cmdLine = parser.parse(options, args);
+
+				if (cmdLine.hasOption("h")) {
+					new HelpFormatter().printHelp("tp.pr3.Main [-g <game>] [-h] [-x <columnNumber>] [-y <rowNumber>]", options);
+
+				}
+				if (cmdLine.hasOption("g")) {
+					game = cmdLine.getOptionValue("game");
+					if (game == "c4") {
+						ok=true;
+					} else if (game == "co") {
+						f= new FactoriaComplica();
+						ok=true;
+					} else if (game == "gr") {
+						if (cmdLine.hasOption("x") && cmdLine.hasOption("y")){
+							
+							columnNumber=Integer.parseInt(cmdLine.getOptionValue("x"));
+							rowNumber=Integer.parseInt(cmdLine.getOptionValue("y"));
+							f= new FactoriaGravity(rowNumber,columnNumber);
+							ok=true;
+							
+						}
+						
+					}
+
+				}
+				if (ok){
+					reglas = f.creaReglas();
+					p = new Partida(reglas);
+					c = new Controlador(f, p, in);
+					c.run();
+				}
+			}  catch (ParseException ex){
+				System.err.println(ex.getMessage());
+				new HelpFormatter().printHelp("h", options);
+			}
+		
+	}
+	
+	
+	
+	private static Options generateOptions(){
+		
 		Options options = new Options();
+		
 		options.addOption("h", "help", false, "Muestra esta ayuda.");
 		options.addOption("g", "game", true,
 				"Tipo de juego (c4, co, gr). Por defecto, c4.");
@@ -30,58 +89,14 @@ public class Main {
 				"Número de columnas del tablero (sólo para Gravity). Por defecto, 10.");
 		options.addOption("y", "tamY", true,
 				"Número de filas del tablero (sólo para Gravity). Por defecto, 10.");
-
-		FactoriaTipoJuego f = new FactoriaConecta4();
-		ReglasJuego reglas = f.creaReglas();
-		Scanner in = new Scanner(System.in);
-		Partida p = new Partida(reglas);
-		Controlador c = new Controlador(f, p, in);
-
-		// aqui irian los optionGroup para incompatibilidades de argumentos
-		if (args == null || args.length < 1 || args[0] == null) {
-			c.run();
-		} else
-			try {
-				parser = new BasicParser();
-				cmdLine = parser.parse(options, args);
-
-				if (cmdLine.hasOption("h")) {
-					new HelpFormatter()
-							.printHelp(
-									"tp.pr3.Main [-g <game>] [-h] [-x <columnNumber>] [-y <rowNumber>]",
-									options);
-
-				}
-				if (cmdLine.hasOption("g")) {
-					game = cmdLine.getOptionValue("game");
-					if (game == "c4") {
-						c.run();
-					} else if (game == "co") {
-						f= new FactoriaComplica();
-						reglas = f.creaReglas();
-						p = new Partida(reglas);
-						c = new Controlador(f, p, in);
-						c.run();
-					} else if (game == "gr") {
-						if (cmdLine.hasOption("x") && cmdLine.hasOption("y")){
-							columnNumber=Integer.parseInt(cmdLine.getOptionValue("x"));
-							rowNumber=Integer.parseInt(cmdLine.getOptionValue("y"));
-							f= new FactoriaGravity(rowNumber,columnNumber);
-							reglas = f.creaReglas();
-							p = new Partida(reglas);
-							c = new Controlador(f, p, in);
-							c.run();
-						}
-						
-					}
-
-				}
-				
-			} catch (Exception e) {
-				System.out.println(cmdLine.getArgs().toString());
-				salida=1;
-			}
-		return salida;
+		
+		options.getOption("g").setArgName("game");
+		options.getOption("x").setArgName("columnNumber");
+		options.getOption("y").setArgName("rowNumber");
+		return options;
+		
+		
+		
 	}
 
 }
